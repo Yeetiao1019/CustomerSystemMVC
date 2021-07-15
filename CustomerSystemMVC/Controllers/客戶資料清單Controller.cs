c8c114133aefde3784e3ec438e73c97a29145ac5
Yeetiao Chen <n1533512@gmail.com>
2021-07-16 01:38:41 +0800

    新增客戶View表、新增客戶資料清單Controller與View、_Layout 新增"客戶資料清單"連結

---
+++ CustomerSystemMVC/Controllers/客戶資料清單Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerSystemMVC.Models;
+
+namespace CustomerSystemMVC.Controllers
+{
+    public class 客戶資料清單Controller : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: 客戶資料清單
+        public ActionResult Index()
+        {
+            return View(db.客戶View表.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
