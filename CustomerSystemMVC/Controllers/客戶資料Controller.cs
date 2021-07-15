4c0b7ef17b29c301a771968ec01937b860248a81
Yeetiao Chen <n1533512@gmail.com>
2021-07-15 23:59:53 +0800

    新增Controller，加入 ValueInjector 套件，新增客戶資料的Create、Edit ViewModel，_layout.cshtml 修改 navbar 連結

---
+++ CustomerSystemMVC/Controllers/客戶資料Controller.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerSystemMVC.Models;
+using CustomerSystemMVC.ViewModels;
+using Omu.ValueInjecter;
+
+namespace CustomerSystemMVC.Controllers
+{
+    public class 客戶資料Controller : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: 客戶資料
+        public ActionResult Index()
+        {
+            return View(db.客戶資料.ToList());
+        }
+
+        // GET: 客戶資料/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: 客戶資料/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: 客戶資料/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(客戶資料Create 客戶資料Create)
+        {
+            客戶資料 客戶資料 = new 客戶資料();
+            if (ModelState.IsValid)
+            {
+                客戶資料.客戶名稱 = 客戶資料Create.客戶名稱;
+                客戶資料.統一編號 = 客戶資料Create.統一編號;
+                客戶資料.電話 = 客戶資料Create.電話;
+                客戶資料.傳真 = 客戶資料Create.傳真;
+                客戶資料.地址 = 客戶資料Create.地址;
+                客戶資料.Email = 客戶資料Create.Email;
+
+                db.客戶資料.Add(客戶資料);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    foreach (var eves in ex.EntityValidationErrors)
+                    {
+                        foreach (var ves in eves.ValidationErrors)
+                        {
+                            throw new Exception(ves.PropertyName + " : " + ves.ErrorMessage);
+                        }
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料Create);
+        }
+
+        // GET: 客戶資料/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            客戶資料Edit 客戶資料Edit = new 客戶資料Edit();
+            客戶資料Edit.客戶名稱 = 客戶資料.客戶名稱;
+            客戶資料Edit.統一編號 = 客戶資料.統一編號;
+            客戶資料Edit.電話 = 客戶資料.電話;
+            客戶資料Edit.傳真 = 客戶資料.傳真;
+            客戶資料Edit.地址 = 客戶資料.地址;
+            客戶資料Edit.Email = 客戶資料.Email;
+
+            return View(客戶資料Edit);
+        }
+
+        // POST: 客戶資料/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int? id,客戶資料Edit 客戶資料Edit)
+        {
+            if (ModelState.IsValid)
+            {
+                客戶資料 客戶資料 = db.客戶資料.Find(id);
+                客戶資料.InjectFrom(客戶資料Edit);      //ValueInjector套件，可以幫忙 mapping Model
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    foreach (var eves in ex.EntityValidationErrors)
+                    {
+                        foreach (var ves in eves.ValidationErrors)
+                        {
+                            throw new Exception(ves.PropertyName + " : " + ves.ErrorMessage);
+                        }
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料Edit);
+        }
+
+        // GET: 客戶資料/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: 客戶資料/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            db.客戶資料.Remove(客戶資料);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CustomerSystemMVC/ViewModels/客戶資料Create.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSystemMVC.ViewModels
+{
+    public class 客戶資料Create
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        public string 客戶名稱 { get; set; }
+        [StringLength(8, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        [MinLength(8, ErrorMessage = "請輸入 {1} 個字元")]
+        public string 統一編號 { get; set; }
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        public string 電話 { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        public string 傳真 { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        public string 地址 { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 {1} 個字元")]
+        public string Email { get; set; }
+    }
+}
